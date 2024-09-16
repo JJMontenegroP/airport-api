54c1ed1feae119daf51581fffd14ad4e509bb91e
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-09-15 22:06:09 -0500

    chore: include env vars and declare airport, airline, airline-airport modules
    * chore: auto-load entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,27 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { AirportModule } from "./airport/airport.module";
+import { AirlineModule } from "./airline/airline.module";
+import { AirlineAirportModule } from "./airline-airport/airline-airport.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
       database: process.env.DB_NAME,
-      entities: [],
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
       synchronize: true,
+      type: "postgres",
+      username: process.env.DB_USER,
     }),
+    AirportModule,
+    AirlineModule,
+    AirlineAirportModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
